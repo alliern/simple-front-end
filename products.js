8d5fd2d6822c7e176b9f76757ec38de777b8621a
Allie Neal <allieneal93@gmail.com>
2021-03-22 15:23:25 -0400

    adds get and post requests

--- products.js
+++ products.js
@@ -1,10 +1,53 @@
 /* global Vue, axios */
 axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+
 var app = new Vue({
   el: "#app",
   data: function() {
     return {
-      message: "Hello from JavaScript!",
+      message: "Add some new products!",
+      products: [],
+      name: "",
+      description: "",
+      imageUrl: "",
+      price: ""
+
+
     };
+  
+  },  
+  created() {
+    axios
+      .get("http://localhost:3000/api/products")
+      .then(response => {
+        console.log(response.data);
+        this.products = response.data;
+      });
   },
+  methods: {
+    createProduct: function () {
+      var params ={name: this.name, description: this.description, image_url: this.imageUrl, price: this.price };
+      axios.post("http://localhost:3000/api/products", params).then(response => {
+        console.log(response.data);
+        this.products.push(response.data);
+        this.name = "";
+        this.description = "";
+        this.imageUrl = "";
+        this.price = "";
+      });
+    },
+    updateProduct: function () {
+      var params ={name: this.name, description: this.description, image_url: this.imageUrl, price: this.price };
+      axios.post("http://localhost:3000/api/products", params).then(response => {
+        console.log(response.data);
+        this.products.push(response.data);
+        this.name = "";
+        this.description = "";
+        this.imageUrl = "";
+        this.price = "";
+      });
+    },
+
+    }
+  }
 });
